585828aed25defc7076ff8fa3343525be0e74009
Ditte <Ditte@DESKTOP-QORFADN>
2017-08-30 10:35:33 +0200

    Imælk interface implementeret i Cortado og Latte

--- Kaffe/Cortado.cs
+++ Kaffe/Cortado.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace Kaffe
 {
-    public class Cortado : Kaffe
+    public class Cortado : Kaffe , Imælk
     {
+        /// <summary>
+        /// Hvor meget mælk skal bruges til Cortado
+        /// </summary>
+        /// <returns></returns>
+        public int mlMælk()
+        {
+            return 25;
+        }
 
         /// <summary>
         /// Prisen på Cortado
--- Kaffe/Latte.cs
+++ Kaffe/Latte.cs
@@ -6,8 +6,17 @@ using System.Threading.Tasks;
 
 namespace Kaffe
 {
-    public class Latte : Kaffe
+    public class Latte : Kaffe , Imælk
     {
+        /// <summary>
+        /// Hvor meget mælk skal bruges til Latte
+        /// </summary>
+        /// <returns></returns>
+        public int mlMælk()
+        {
+            return 120;
+        }
+
         public override int Pris()
         {
             return 40;
