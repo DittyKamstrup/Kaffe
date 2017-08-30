6a787e24abc7f774430fffb64fe08182f4b18628
Ditte <Ditte@DESKTOP-QORFADN>
2017-08-30 09:51:39 +0200

    Abstract Kaffe klasse lavet og nedarvning til SortKaffe klasse lavet

---
+++ Kaffe/Kaffe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffe
+{
+    public abstract class Kaffe
+    {
+        // abstract - Skal overskrives
+        public abstract int Pris();
+
+        // virtual - Kan overskrives
+        public virtual string Styrke()
+        {
+            return "Stærk";
+        }
+    }
+}
---
+++ KaffeTests/KaffeTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Kaffe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kaffe.Tests
+{
+    [TestClass()]
+    public class KaffeTests
+    {
+        [TestMethod()]
+        public void PrisTestKaffe()
+        {
+            //Arrange
+            var kaffe = new SortKaffe();
+            //Act
+            int pris = kaffe.Pris();
+            //Assert
+            Assert.AreEqual(20, pris);
+        }
+
+        [TestMethod()]
+        public void StyrkeTestKaffe()
+        {
+            //Arrange
+            var kaffe = new SortKaffe();
+            //Act
+            string styrke = kaffe.Styrke();
+            //Assert
+            Assert.AreEqual("Stærk", styrke);
+        }
+    }
+}
